9f16682d04d3addbf1f79fd9ee8b1ecf3a2c21eb
sangelito <129316135+sangelito@users.noreply.github.com>
2023-06-20 18:51:23 -0500

    GEt route

--- Develop/public/routes/htmlroute.js
+++ Develop/public/routes/htmlroute.js
@@ -11,3 +11,7 @@ router.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
   });
   
+// fall back GET route 
+router.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
