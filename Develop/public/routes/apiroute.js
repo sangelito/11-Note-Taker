fbe37e3f328ca12f5c3c35799304727648dc90a6
sangelito <129316135+sangelito@users.noreply.github.com>
2023-06-20 18:56:31 -0500

    notes path

--- Develop/public/routes/apiroute.js
+++ Develop/public/routes/apiroute.js
@@ -1,2 +1,7 @@
 const { Router } = require("express");
 const { uuid } = require("crypto");
+
+//GET route for notes
+app.get("notes/:id", (req, res) => {
+    const note = parsedNotes[req.params.id];
+  });
