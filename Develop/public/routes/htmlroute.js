d423114451745b06db82c57fec591f2305536c30
sangelito <129316135+sangelito@users.noreply.github.com>
2023-06-20 18:49:30 -0500

    html route

--- Develop/public/routes/htmlroute.js
+++ Develop/public/routes/htmlroute.js
@@ -1,3 +1,8 @@
 const path = require("path");
 const router = require("express").Router();
 
+//Defining GET route/callback function
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+  
