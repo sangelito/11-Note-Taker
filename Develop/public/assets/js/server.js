821f865163a9652fed8aa77c22c237e251d3d386
sangelito <129316135+sangelito@users.noreply.github.com>
2023-06-20 18:45:54 -0500

    json file and assigned it to variable

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -2,3 +2,9 @@ const express = require("express");
 const path = require("path");
 const htmlRoutes = require("./routes/htmlroutes");
 const apiRoutes = require("./routes/apiroutes");
+
+// JSON files = termData
+const { request } = require("http");
+const termData = require("./terms.json");
+const PORT = process.env.PORT || 3001;
+
