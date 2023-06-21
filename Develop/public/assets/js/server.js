a61c455dc94a3052abccee81f8a45225a84f4b1f
sangelito <129316135+sangelito@users.noreply.github.com>
2023-06-20 19:17:19 -0500

    parsing incoming data

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -11,8 +11,14 @@ const PORT = process.env.PORT || 3001;
 // initialize app 
 const app = express (); 
 
-// pase incoming data 
+// parse incoming data 
+app.use(express.urlencoded({ extended: true }));
+//parse JSON data 
+app.use(express.json());
+app.use(express.static("public"));
 
+const parsedNotes = Json.parse(Notes);
+console.info("parsed notes", parsedNotes);
 // route for notes 
 
 // route for api 
