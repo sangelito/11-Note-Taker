0acb9318873159fe3cb5b0e02dc1b08b333aa3be
sangelito <129316135+sangelito@users.noreply.github.com>
2023-06-21 13:18:16 -0500

    post request

--- routes/apiroute.js
+++ routes/apiroute.js
@@ -1,5 +1,8 @@
 // const { Router } = require("express");
 // const { uuid } = require("crypto");
+var db = require("../db/db.json");
+var fs = require("fs");
+
 const path = require("path");
 const router = require("express").Router();
 //GET route for notes
@@ -8,7 +11,22 @@ router.delete("/notes/:id", (req, res) => {
   });
 
 //route to api
-router.get("/", (req, res) => res.json(notes));
+router.get("/notes", (req, res) => res.json(notes));
+
+  // API POST Request
+  router.post("/api/notes", function (req, res) {
+    // Receive a new note to save on the request body, add it to the db.json file
+    db.push(req.body);
+    // Add unique id to each note
+    db.forEach((obj, i) => {
+      obj.id = i + 1;
+    });
+    // Return the new note to the client
+    fs.writeFile("./db/db.json", JSON.stringify(db), function () {
+      res.json(db);
+    });
+  });
 
 // export 
 module.exports = router; 
+
